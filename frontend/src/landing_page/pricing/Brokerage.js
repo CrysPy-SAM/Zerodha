d204177d185bfac3e2dde0b9b4c7920ea1846e54
CrysPy-SAM <ssmishra9918@gmail.com>
2025-10-08 13:14:06 +0530

    Signup form

--- frontend/src/landing_page/pricing/Brokerage.js
+++ frontend/src/landing_page/pricing/Brokerage.js
@@ -1,44 +1,48 @@
 import React from "react";
 
-function LeftSection({
-  imageURL,
-  productName,
-  productDesription,
-  tryDemo,
-  learnMore,
-  googlePlay,
-  appStore,
-}) {
+function Brokerage() {
   return (
-    <div className="container mt-5">
-      <div className="row">
-        <div className="col-6">
-          <img src={imageURL} />
+    <div className="container">
+      <div className="row p-5 mt-5 text-center border-top">
+        <div className="col-8 p-4">
+          <a href="" style={{ textDecoration: "none" }}>
+            <h3 className="fs-5">Brokerage calculator</h3>
+          </a>
+          <ul
+            style={{ textAlign: "left", lineHeight: "2.5", fontSize: "12px" }}
+            className="text-mut"
+          >
+            <li>
+              Call & Trade and RMS auto-squareoff:Additional charges of ₹50 +
+              GST per order.
+            </li>
+            <li>Digital contract notes will be sent via e-mail.</li>
+            <li>
+              Physical copies of contract notes, if required, shall be charged
+              ₹20 per contract note. Courier charges apply.
+            </li>
+            <li>
+              For NRI account (non-PIS), 0.5% or ₹100 per executed order for
+              equity (whichever is lower).
+            </li>
+            <li>
+              For NRI account (PIS), 0.5% or ₹200 per executed order for equity
+              (whichever is lower).
+            </li>
+            <li>
+              If the account is in debit balance, any order placed will be
+              charged ₹40 per executed order instead of ₹20 per executed order.
+            </li>
+          </ul>
         </div>
-        <div className="col-6 p-5 mt-5">
-          <h1>{productName}</h1>
-          <p>{productDesription}</p>
-          <div>
-            <a href={tryDemo}>Try Demo</a>
-            <a href={learnMore} style={{ marginLeft: "50px" }}>
-              Learn More
-            </a>
-          </div>
-          <div className="mt-3">
-            <a href={googlePlay}>
-              <img src="media/images/googlePlayBadge.svg" />
-            </a>
-            <a href={appStore}>
-              <img
-                src="media/images/appstoreBadge.svg"
-                style={{ marginLeft: "50px" }}
-              />
-            </a>
-          </div>
+        <div className="col-4 p-4">
+          <a href="" style={{ textDecoration: "none" }}>
+            <h3 className="fs-5">List of charges</h3>
+          </a>
         </div>
       </div>
     </div>
   );
 }
 
-export default LeftSection;
+export default Brokerage;
